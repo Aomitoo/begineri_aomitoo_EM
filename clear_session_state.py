f4b1a90db1eddfaae43ae96fc637fe31699431d4
Aomitoo <werodoratempest@gmail.com>
2025-01-07 18:57:06 +0500

    Update clear_session_state.py

--- clear_session_state.py
+++ clear_session_state.py
@@ -5,6 +5,7 @@ def clear_session_state(session_state):
         del session_state['send_email']
     if 'name_mail' in session_state:
         del session_state['name_mail']
+    if 'bind_email' in session_state:
+        del session_state['bind_email']
     
     session_state = {}
-    return "Ладно, чего изволите?"
